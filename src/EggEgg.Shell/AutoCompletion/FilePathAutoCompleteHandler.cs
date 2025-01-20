58c868fc023832586bcdf50b066fa7f0390514f0
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2025-01-21 02:33:24 +0800

    FilePath completer bugfix

--- src/EggEgg.Shell/AutoCompletion/FilePathAutoCompleteHandler.cs
+++ src/EggEgg.Shell/AutoCompletion/FilePathAutoCompleteHandler.cs
@@ -30,37 +30,55 @@ public class FilePathAutoCompleteHandler : IAutoCompleteHandler
 
         // unclosed " trigger the file-name completions
         var leftCount = left.Count(c => c == '"');
-        // Log.Info($"left: '{left}', right: '{right}', endlimit: '{endlimit}', leftCount: '{leftCount}'");
+        Log.Info($"left: '{left}', right: '{right}', endlimit: '{endlimit}', leftCount: '{leftCount}'");
         if (leftCount % 2 != 1) return new();
         if (endlimit.IndexOfAny(GetPathSeparators()) >= 0) return new();
 
         var startIndex = left.LastIndexOf('"') + 1;
         Debug.Assert(startIndex > 0);
         var endIndex = text.IndexOf('"', index);
-        // Log.Info($"index: [{startIndex}, {endIndex})");
+        Log.Info($"index: [{startIndex}, {endIndex})");
 
         var requestedPath = left[startIndex..];
         var separatorIdx = requestedPath.LastIndexOfAny(GetPathSeparators());
-        // Log.Info($"requestedPath: '{requestedPath}', separatorIdx: {separatorIdx}");
-        var inputDir = separatorIdx == -1 ? requestedPath : requestedPath[..separatorIdx];
-        if (inputDir == string.Empty) inputDir = ".";
+        Log.Info($"requestedPath: '{requestedPath}', separatorIdx: {separatorIdx}");
+        var inputDir = requestedPath[..(separatorIdx + 1)];
+        if (inputDir == string.Empty) inputDir = $".{Path.DirectorySeparatorChar}";
 
         var parentDir = Path.GetFullPath(inputDir, CurrentPath);
         var startlimit = requestedPath[(separatorIdx + 1)..];
-        // Log.Info($"parentDir: '{parentDir}'', startlimit: {startlimit}");
+        Log.Info($"parentDir: '{parentDir}'', startlimit: {startlimit}");
 
-        var enumeratedNames = Directory.EnumerateDirectories(parentDir, "*", SearchOption.TopDirectoryOnly)
-            .Concat(Directory.EnumerateFiles(parentDir, "*.*", SearchOption.TopDirectoryOnly))
-            .Select(x => Path.GetFileName(x));
-        var names = from name in enumeratedNames
-                    where name.StartsWith(startlimit) && name.EndsWith(endlimit)
-                    select $"{inputDir}{Path.DirectorySeparatorChar}{name}";
-        // Log.Info($"names: {names}");
-        return new()
+        try
         {
-            Suggestions = names.ToList(),
-            StartIndex = startIndex,
-            EndIndex = endIndex,
-        };
+            var enumeratedNames = Directory.EnumerateDirectories(parentDir, "*", SearchOption.TopDirectoryOnly)
+                .Concat(Directory.EnumerateFiles(parentDir, "*.*", SearchOption.TopDirectoryOnly))
+                .Select(x => Path.GetFileName(x));
+            var names = from name in enumeratedNames
+                        where name.StartsWith(startlimit, StringComparison.OrdinalIgnoreCase) && name.EndsWith(endlimit)
+                        select $"{inputDir[..^1]}{Path.DirectorySeparatorChar}{name}";
+            Log.Info($"names: {string.Join(',', names)}");
+            return new()
+            {
+                Suggestions = names.ToList(),
+                StartIndex = startIndex,
+                EndIndex = endIndex,
+            };
+        }
+        catch (Exception ex)
+        {
+            if (ex is IOException ioex2) Log.Info(ioex2.HResult.ToString());
+            if (ex is DirectoryNotFoundException)
+            {
+                Log.Warn($"The specified directory does not exist. ('{parentDir}')", nameof(FilePathAutoCompleteHandler));
+                return new();
+            }
+            else if (ex is IOException ioex && ((uint)ioex.HResult == 0x80070057 || (uint)ioex.HResult == 0x80070003))
+            {
+                Log.Warn($"You're trying to query subentries of a file. Consider remove the last '{inputDir[^1]}'. ('{inputDir}')", nameof(FilePathAutoCompleteHandler));
+                return new();
+            }
+            else throw;
+        }
     }
 }
